fcd7771c0b61f012ad4c84f30e32fca4803ddeaa
H.Yamada <ymd_h@outlook.jp>
2025-09-22 15:18:46 +0900

    Format code

--- tests/test_lerobot.py
+++ tests/test_lerobot.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from lazy_robot_loader.lerobot import LeRobotDataset
 
+
 def test_pushT(benchmark):
     data = LeRobotDataset(repo_id="lerobot/pusht@v2.1")
 
@@ -15,6 +16,7 @@ def test_pushT(benchmark):
         return data[i]
 
     benchmark(run, np.asarray(0))
+
 
 def test_pushT_5obs(benchmark):
     data = LeRobotDataset(
@@ -30,4 +32,3 @@ def test_pushT_5obs(benchmark):
         return data[i]
 
     benchmark(run, np.asarray(0))
-
