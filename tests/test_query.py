ad38c842f660d0de55d1006821cb52e9b7ef0286
H.Yamada <ymd_h@outlook.jp>
2025-10-04 22:43:04 +0900

    Add aggregation test

---
+++ tests/test_query.py
@@ -0,0 +1,22 @@
+import duckdb
+
+from lazy_robot_loader.lerobot.internal.query import agg_stats
+
+
+def test_agg_stats():
+    con = duckdb.connect()
+
+    mu, _ = agg_stats("mu", "sigma", "count")
+
+    m = con.query(f"""
+    SELECT {mu} AS "mu",
+    FROM (
+      SELECT
+        unnest([10.0, 20.0, 30.0]) AS "mu",
+        unnest([1.0, 2.0, 3.0]) AS "sigma",
+        unnest([5, 15, 5]) AS "count",
+    );
+    """).fetch_arrow_table()
+
+    assert m.num_rows == 1
+    assert m.num_columns == 1
