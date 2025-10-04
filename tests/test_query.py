e5905c6714b63c499f07ab2e3e9a8008d22ca6a7
H.Yamada <ymd_h@outlook.jp>
2025-10-04 23:53:13 +0900

    Add agg_vector test

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,6 +1,27 @@
 import duckdb
+import numpy as np
 
-from lazy_robot_loader.lerobot.internal.query import agg_stats
+from lazy_robot_loader.lerobot.internal.query import agg_vector, agg_stats
+from lazy_robot_loader.lerobot.internal.functional import to_array
+
+
+def test_agg_vector():
+    con = duckdb.connect()
+
+    m = agg_vector("max", "a", 3)
+
+    mm = to_array(
+        con.query(f"""
+    SELECT {m} AS "max",
+    FROM (
+      SELECT
+        unnest([[1.0, 2.0, 3.0], [4.0, 5.0, 7.0], [-1.2, 9.2, 3.4]]) AS "a"
+    );
+    """).fetch_arrow_table()["max"]
+    )
+
+    assert mm.shape == (1, 3)
+    np.testing.assert_allclose(mm, np.asarray([[4.0, 9.2, 7.0]]))
 
 
 def test_agg_stats():
